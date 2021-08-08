a788865f425e524fef8c5c1c95f0b205f9f82f7b
Carolina Daniel <arq.carolinadaniel@gmail.com>
2021-08-08 20:27:44 -0300

    Mandatory

--- get_next_line.c
+++ get_next_line.c
@@ -6,41 +6,42 @@
 /*   By: cado-car <cado-car@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/08 17:44:55 by cado-car          #+#    #+#             */
-/*   Updated: 2021/08/08 19:58:52 by cado-car         ###   ########lyon.fr   */
+/*   Updated: 2021/08/08 20:26:51 by cado-car         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-int	read_file(int fd, char *buffer, char *line_read);
-void	put_line(char *line_read);
+int		read_file(int fd, char *buffer, char *line_read);
+void	put_line(char *line_read, char *line);
 
 char *get_next_line(int fd)
 {
-	static char	*line_read = NULL;
+	static char	line_read[1] = "\0";
 	char		*buffer;
+	char		*line;
 
 	if (fd < 0 || BUFFER_SIZE <= 0)
 		return (NULL);
 	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buffer)
 		return (NULL);
+	ft_bzero(buffer, BUFFER_SIZE + 1);
 	if (!ft_strchr(line_read, '\n'))
 	{
 		if (!read_file(fd, buffer, line_read))
 			return (NULL);
-		free(buffer);		
 	}
-	put_line(line_read);
-	return (line_read);
+	free(buffer);		
+	put_line(line_read, line);
+	return (line);
 }
 
 int	read_file(int fd, char *buffer, char *line_read)
 {
 	ssize_t n;
 
-	while (!ft_strchr(buffer, '\n'))
+	while (!ft_strchr(line_read, '\n'))
 	{
-		ft_bzero(buffer, BUFFER_SIZE + 1);
 		n = read(fd, buffer, BUFFER_SIZE);
 		if (n < 0)
 			return (0);
@@ -48,12 +49,12 @@ int	read_file(int fd, char *buffer, char *line_read)
 			return (1);	
 		if (n > 0)
 			line_read = ft_strjoin(line_read, buffer);
+		ft_bzero(buffer, BUFFER_SIZE + 1);
 	}
-	ft_bzero(buffer, BUFFER_SIZE + 1);
 	return (1);
 }
 
-void	put_line(char *line_read)
+void	put_line(char *line_read, char *line)
 {
 	size_t	i;
 	char	nl;
@@ -66,5 +67,9 @@ void	put_line(char *line_read)
 		i++;
 	}
 	write(1, &nl, 1);
+	line = (char *)malloc((i + i) * sizeof(char));
+	if (!line)
+		return (NULL);
+	ft_strlcpy(line, line_read, i);
 	line_read += i;
 }
