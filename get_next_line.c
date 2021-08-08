8dd8beb126ab7aa4a5fefdd80b5a33b2cd409970
Carolina Daniel <arq.carolinadaniel@gmail.com>
2021-08-08 20:54:28 -0300

    Mandatory

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cado-car <cado-car@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/08 17:44:55 by cado-car          #+#    #+#             */
-/*   Updated: 2021/08/08 20:43:09 by cado-car         ###   ########lyon.fr   */
+/*   Updated: 2021/08/08 20:54:19 by cado-car         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 int		read_file(int fd, char *buffer, char *line_read);
 char	*put_line(char *line_read);
 
-char *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
 	static char	line_read[1] = "\0";
 	char		*buffer;
@@ -31,14 +31,15 @@ char *get_next_line(int fd)
 		if (!read_file(fd, buffer, line_read))
 			return (NULL);
 	}
-	free(buffer);		
+	free(buffer);
 	line = put_line(line_read);
 	return (line);
 }
 
 int	read_file(int fd, char *buffer, char *line_read)
 {
-	ssize_t n;
+	ssize_t	n;
+	char	*temp;
 
 	while (!ft_strchr(line_read, '\n'))
 	{
@@ -46,9 +47,13 @@ int	read_file(int fd, char *buffer, char *line_read)
 		if (n < 0)
 			return (0);
 		if (n == 0)
-			return (1);	
+			return (1);
 		if (n > 0)
+		{
+			temp = line_read;
 			line_read = ft_strjoin(line_read, buffer);
+			free(temp);
+		}
 		ft_bzero(buffer, BUFFER_SIZE + 1);
 	}
 	return (1);
@@ -59,7 +64,7 @@ char	*put_line(char *line_read)
 	size_t	i;
 	char	nl;
 	char	*line;
-	
+
 	i = 0;
 	nl = '\n';
 	while (line_read[i] != '\n')
