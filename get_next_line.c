5a7e7c5f74b9be6dbae224a12e6da498cff374c7
Carolina Daniel <arq.carolinadaniel@gmail.com>
2021-08-10 14:16:02 -0300

    Mandatory

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cado-car <cado-car@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/08 17:44:55 by cado-car          #+#    #+#             */
-/*   Updated: 2021/08/10 14:15:38 by cado-car         ###   ########lyon.fr   */
+/*   Updated: 2021/08/10 14:15:59 by cado-car         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,7 +46,7 @@ ssize_t	read_file(int fd, char **buffer, char **buff_read, char **line)
 		if (n < 0)
 		{
 			free(*buff_read);
-			*buff_read == NULL;
+			*buff_read = NULL;
 			free(*buffer);
 			return (n);
 		}
