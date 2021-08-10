6430698b28d1ea217ae4265b0bd75eb479ec6f22
Carolina Daniel <arq.carolinadaniel@gmail.com>
2021-08-10 14:05:17 -0300

    Mandatory

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,13 @@
 /*   By: cado-car <cado-car@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/08 17:44:55 by cado-car          #+#    #+#             */
-/*   Updated: 2021/08/10 12:01:23 by cado-car         ###   ########lyon.fr   */
+/*   Updated: 2021/08/10 14:02:46 by cado-car         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-ssize_t	read_file(int fd, char **buffer, char *buff_read, char **line);
-char	*get_line(char *buff_read, char **line);
+ssize_t	read_file(int fd, char **buffer, char **buff_read, char **line);
+char	*get_line(char **buff_read, char **line);
 
 char	*get_next_line(int fd)
 {
@@ -28,35 +28,35 @@ char	*get_next_line(int fd)
 	buffer = (char *)malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (!buffer)
 		return (NULL);
-	n = read_file(fd, &buffer, buff_read, &line);
+	n = read_file(fd, &buffer, &buff_read, &line);
 	if (n < 0 || (n == 0 && !line))
 		return (NULL);
 	return (line);
 }
 
-ssize_t	read_file(int fd, char **buffer, char *buff_read, char **line)
+ssize_t	read_file(int fd, char **buffer, char **buff_read, char **line)
 {
 	char	*temp;
 	ssize_t	n;
 
 	n = 1;
-	while (!ft_strchr(buff_read, '\n') && n)
+	while (!ft_strchr(*buff_read, '\n') && n)
 	{
 		n = read(fd, *buffer, BUFFER_SIZE);
 		if (n < 0)
 		{
-			free(buff_read);
+			free(*buff_read);
 			free(*buffer);
 			return (n);
 		}
 		(*buffer)[n] = '\0';
-		temp = buff_read;
-		buff_read = ft_strjoin(temp, *buffer);
+		temp = *buff_read;
+		*buff_read = ft_strjoin(temp, *buffer);
 		free(temp);
 	}
 	free(*buffer);
 	*buffer = NULL;
-	buff_read = get_line(buff_read, line);
+	*buff_read = get_line(buff_read, line);
 	if (**line == '\0')
 	{
 		free(*line);
@@ -65,23 +65,24 @@ ssize_t	read_file(int fd, char **buffer, char *buff_read, char **line)
 	return (n);
 }
 
-char	*get_line(char *buff_read, char **line)
+char	*get_line(char **buff_read, char **line)
 {
 	size_t	i;
 	char	*new_buff;
 
 	i = 0;
 	new_buff = NULL;
-	while ((*(buff_read + i) != '\n') && (*(buff_read + i) != '\0'))
+	while ((*(*buff_read + i) != '\n') && (*(*buff_read + i) != '\0'))
 		i++;
-	if (*(buff_read + i) == '\n')
+	if (*(*buff_read + i) == '\n')
 	{
 		i++;
-		*line = ft_substr(buff_read, 0, i);
-		new_buff = ft_strdup(buff_read + i);
+		*line = ft_substr(*buff_read, 0, i);
+		new_buff = ft_strdup(*buff_read + i);
 	}
 	else
-		*line = ft_strdup(buff_read);
-	free(buff_read);
+		*line = ft_strdup(*buff_read);
+	free(*buff_read);
+	*buff_read = NULL;
 	return (new_buff);
 }
