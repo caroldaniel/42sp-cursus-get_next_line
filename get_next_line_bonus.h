2de7dcf8e35f5b43a07bbc29f1dc971d15b877bc
Carolina Daniel <arq.carolinadaniel@gmail.com>
2021-08-10 19:51:36 -0300

    Bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cado-car <cado-car@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/10 18:50:22 by cado-car          #+#    #+#             */
+/*   Updated: 2021/08/10 19:45:14 by cado-car         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <limits.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1000
+# endif
+
+char	*get_next_line(int fd);
+
+// utils
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strdup(const char *s1);
+char	*ft_substr(char const *s, unsigned int start, size_t len);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,135 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cado-car <cado-car@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/08/10 18:50:25 by cado-car          #+#    #+#             */
+/*   Updated: 2021/08/10 18:52:15 by cado-car         ###   ########lyon.fr   */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+/* 
+*	LIBFT FUNCTIONS
+*	---------------
+*	To view full documentation, check the libft repo for the following:
+*	#1. ft_strchr.c
+*	#2. ft_strlcpy.c
+*	#3. ft_strjoin.c
+*	#4. ft_strdup.c
+*	#1. ft_substr.c
+*/
+
+char	*ft_strchr(const char *s, int c)
+{
+	int		i;
+	char	ch;
+
+	ch = (unsigned char)c;
+	i = 0;
+	if (!s)
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		if (s[i] == ch)
+			return ((char *)(&s[i]));
+		i++;
+	}
+	if (ch == '\0')
+		return ((char *)&s[i]);
+	return (NULL);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+	size_t	count;
+
+	if (!dst || !src)
+		return (0);
+	i = 0;
+	count = 0;
+	while (src[count] != '\0')
+		count++;
+	if (dstsize != 0)
+	{
+		while (src[i] != '\0' && i < (dstsize - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}	
+	return (count);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*ccat;
+	int		len_s1;
+	int		len_s2;
+
+	if (!s1 || !s2)
+		return (NULL);
+	len_s1 = 0;
+	while (s1[len_s1] != '\0')
+		len_s1++;
+	len_s2 = 0;
+	while (s2[len_s2] != '\0')
+		len_s2++;
+	ccat = (char *)malloc((len_s1 + len_s2 + 1) * sizeof(char));
+	if (ccat == NULL)
+		return (NULL);
+	ft_strlcpy(&ccat[0], s1, len_s1 + 1);
+	ft_strlcpy(&ccat[len_s1], s2, len_s2 + 1);
+	return (ccat);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	size_t	size;
+	size_t	i;
+	char	*ptr;
+
+	if (!s1)
+		return (NULL);
+	size = 0;
+	while (s1[size] != '\0')
+		size++;
+	ptr = malloc((size + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	while (i < size)
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	ptr[size] = '\0';
+	return (ptr);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sstr;
+	size_t	strlen;
+
+	if (s == NULL)
+		return (NULL);
+	strlen = 0;
+	while (s[strlen] != '\0')
+		strlen++;
+	if (start > strlen)
+		return (ft_strdup(""));
+	if (strlen - start >= len)
+		sstr = (char *)malloc((len + 1) * sizeof(char));
+	else
+		sstr = (char *)malloc((strlen - start + 1) * sizeof(char));
+	if (sstr == NULL)
+		return (NULL);
+	ft_strlcpy(sstr, (s + start), (len + 1));
+	return (sstr);
+}
